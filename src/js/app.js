2729eb7afbc5436b78e8946695abb7984f36196b
xm1ls <xm1ls2000@gmail.com>
2023-11-13 12:51:03 +0200

    finish gulp assembly

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import * as myFunctions from './modules/functions.js';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper();
+myFunctions.isWebp();
+// alert("hello");
