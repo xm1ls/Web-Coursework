679c40e791cb7c97711534a0cf11fe5a8dc21c22
xm1ls <xm1ls2000@gmail.com>
2023-12-10 03:36:56 +0200

    add adaptive hero section

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -11,12 +11,12 @@ const sass = gulpSass(dartSass);
 
 export const scss = () => {
     return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
-        .pipe(app.plugins.plumber(
-            app.plugins.notify.onError({
-                title: 'SCSS',
-                message: "Error: <%= error.message %>"
-            })
-        ))
+        // .pipe(app.plugins.plumber(
+        //     app.plugins.notify.onError({
+        //         title: 'SCSS',
+        //         message: "Error: <%= error.message %>"
+        //     })
+        // ))
         .pipe(app.plugins.replace(/@img\//g, '../img/'))
         .pipe(sass({
             outputStyle: 'expanded'
