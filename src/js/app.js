041ed16551ca4ac902c35128808f8bb685bfc435
xm1ls <xm1ls2000@gmail.com>
2023-12-22 01:03:11 +0200

    smooth scroll to sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as myFunctions from './modules/functions.js';
+import * as anchor from './modules/anchors.js';
 import { Navigation, Pagination } from 'swiper/modules';
 
 const swiper = new Swiper();
