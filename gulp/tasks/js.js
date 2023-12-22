0c3451c59306db8eab0fbcde944deff325ca4d7c
xm1ls <xm1ls2000@gmail.com>
2023-12-22 20:41:41 +0200

    add counter in 'achievements' section

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,12 +2,12 @@ import webpack from 'webpack-stream';
 
 export const js = () => {
     return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
-    .pipe(app.plugins.plumber(
-        app.plugins.notify.onError({
-            title: 'JS',
-            message: "Error: <%= error.message %>"
-        })
-    ))
+    // .pipe(app.plugins.plumber(
+    //     app.plugins.notify.onError({
+    //         title: 'JS',
+    //         message: "Error: <%= error.message %>"
+    //     })
+    // ))
     .pipe(webpack({
         mode: app.isBuild ? 'production' : 'development',
         output: {
--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-import * as myFunctions from './modules/functions.js';
-// import * as anchor from './modules/anchors.js';
-import { Navigation, Pagination } from 'swiper/modules';
+// import * as myFunctions from './modules/functions.js';
+// import { Navigation, Pagination } from 'swiper/modules';
 
-const swiper = new Swiper();
-myFunctions.isWebp();
-// alert("hello");
+import * as jquery from './counter/jquery.min.js';
+import * as jquery_easing from './counter/jquery.easing.js';
+import * as counter from './counter/counter.min.js';
+import count from './counter/count.js';
