dbad4b0b855e64e9e74d2c60bfdb1193af1e55e0
xm1ls <xm1ls2000@gmail.com>
2023-12-27 01:04:49 +0200

    fix swiper on 'community-updates' section

--- src/js/swiper/community_updates_swiper.js
+++ src/js/swiper/community_updates_swiper.js
@@ -4,14 +4,22 @@ let swiper = new Swiper('.swiper', {
 
     breakpoints: {
 
+        0: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0
+        },
+
         768: {
             slidesPerView: 2,
             slidesPerGroup: 2,
+            spaceBetween: 20
         },
 
         1200: {
             slidesPerView: 3,
             slidesPerGroup: 3,
+            spaceBetween: 30
         }
     },
 
