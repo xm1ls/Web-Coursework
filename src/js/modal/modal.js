adf9c9b438dc06c6fc32f43072cd7e4830524096
xm1ls <xm1ls2000@gmail.com>
2023-12-26 19:13:50 +0200

    add modal window to 'footer' input

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,14 +1,25 @@
 (() => {
-    let refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  let refs = {
+    openModalBtns: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    inputField: document.querySelector("[data-input-field]")
+  };
+
+  refs.openModalBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      toggleModal();
+      focusInputField();
+    });
+  });
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function focusInputField() {
+    refs.inputField.focus();
+  }
+})();
